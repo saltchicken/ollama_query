4e7e717e684ae9dcc2b1f49f31df116a95702a67
saltchicken <john.eicher89@gmail.com>
2025-03-15 13:02:32 -0700

    Fix debug_string

--- src/ollama_query/ollama_query.py
+++ src/ollama_query/ollama_query.py
@@ -1,6 +1,6 @@
 import requests
 
-def ollama_query(model, prompt, system_message=None, verbose=False, host="localhost", port=11434):
+def ollama_query(model, prompt, system_message=None, host="localhost", port=11434):
     url = f"http://{host}:{port}/api/generate"
     headers = {"Content-Type": "application/json"}
     data = {
@@ -14,21 +14,24 @@ def ollama_query(model, prompt, system_message=None, verbose=False, host="localh
     response = requests.post(url, headers=headers, json=data)
     response = response.json()
 
-    if verbose: pretty_print_prompt(prompt, system_message, response)
-    return response.get("response", None)
+    debug_string = pretty_print_prompt(prompt, system_message, response)
+    return response.get("response", None), debug_string
 
 def pretty_print_prompt(prompt, system_message, response):
-    print("-------SYSTEM MESSAGE--------")
-    print(system_message)
-    print("----------PROMPT---------")
-    print(prompt)
-    print("----------RESPONSE---------")
-    print(response)
-    print("\n\n")
     if system_message:
-        print(f"Estimated tokens: {estimate_token_length(system_message) + estimate_token_length(prompt)}")
+        estimated_tokens =  estimate_token_length(system_message) + estimate_token_length(prompt)
     else:
-        print(f"Estimated tokens: {estimate_token_length(prompt)}")
+        estimated_tokens = estimate_token_length(prompt)
+    debug_string = f"""
+    Estimated tokens: {estimated_tokens}
+    -------SYSTEM MESSAGE--------
+     {system_message}
+    ----------PROMPT---------
+     {prompt}
+    ----------RESPONSE---------
+     {response}
+    """
+    return debug_string
 
 def estimate_token_length(text: str) -> int:
     """Estimate the number of tokens in a string."""
